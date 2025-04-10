e09ad124ff3184802649d8360213aa89e2954e69
tincho20087 <48802391@est.ort.edu.ar>
2025-04-10 12:07:32 -0300

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class NewBehaviourScript: MonoBehaviour
 {
+    public string GustoHelado;
+    public float CantHelado;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
 
+    {
+        if (CantHelado < 250 || CantHelado > 3000)
+        {
+            Debug.Log("Cant helado no valida");
+            return;
+        }
+        if (GustoHelado != "CHO" && GustoHelado != "DDL" && GustoHelado != "FRU")
+        {
+            Debug.Log("Sabor de helado no disponible");
+        }
+        else if (GustoHelado == "CHO" || GustoHelado == "DDL")
+        {
+            CantHelado = CantHelado * 0.5f;
+            Debug.Log("El precio del helado de " + GustoHelado + " tiene un precio de " + CantHelado);
+        }
+        else if (GustoHelado == "FRU")
+        {
+            CantHelado = CantHelado * 0.5f;
+            CantHelado = CantHelado * 0.9f;
+            Debug.Log("El precio del helado de " + GustoHelado + " tiene un precio de " + CantHelado);
+        }
+        else
+        {
+            return;
+        }
+
+
+
+
+    }
     // Update is called once per frame
+
     void Update()
     {
-        
+
+
     }
 }
+
